c8303833abf90f61c33e3e23aaa7909ecdb18a17
Zemphix <vafinaaa51@gmail.com>
2024-06-11 06:04:32 +0500

    added function to get balance

--- scripts/script.py
+++ scripts/script.py
@@ -1,4 +1,28 @@
-import pybit
+from pybit.unified_trading import HTTP
 import logging
 from dotenv import load_dotenv
 import os
+
+
+dotenv_path = os.path.join('config', '.env')
+load_dotenv(dotenv_path=dotenv_path)
+    
+API_KEY = os.getenv("API_KEY")
+API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+accountType = os.getenv("ACCOUNT_TYPE")
+
+client = HTTP(
+    testnet=False,
+    api_key=API_KEY,
+    api_secret=API_KEY_SECRET
+)
+logging.basicConfig(level=logging.INFO, filename='log.txt', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def get_balance():
+    try:
+        balance = client.get_wallet_balance(accountType=accountType)
+        return f"{balance['result']['list'][0]['coin'][0]['walletBalance']} {balance['result']['list'][0]['coin'][0]['coin']}"
+    except Exception as e:
+        logging.error(e)
+        return "ErrorCode 401"
