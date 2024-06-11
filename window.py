bb88ee1230b05b0edb8e683e48fa2bd8b0e66c96
Zemphix <vafinaaa51@gmail.com>
2024-06-11 06:06:05 +0500

    added activate, stop and bot settings buttons

--- window.py
+++ window.py
@@ -1,16 +1,26 @@
 import dearpygui.dearpygui as dpg
 import json, os
-from callback.callbacks import *
+from callbacks.callbacks import *
+from scripts import script
+import windows
+import windows.bot_settings
 
 
 dpg.create_context()
 
-with dpg.window(label="main window", width=1, height=1, no_resize=True) as main_window:
+
+
+with dpg.window(label="main window", width=1, height=1, no_resize=True, tag='main_window') as main_window:
     with dpg.group(horizontal=True):
         dpg.add_button(label="settings", callback=settings_window)
-    with dpg.child_window():
-        pass
+        dpg.add_button(label="bot settings", callback=windows.bot_settings.bot_settings_window)
+        dpg.add_button(label="activate")
+        dpg.add_button(label="stop")
+    with dpg.child_window(tag='main_window_child'):         
+        balance_widget(script.get_balance())
+        
 
+        
 
 dpg.create_viewport(title='Ihorium', width=1300, height=700, resizable=False)
 dpg.setup_dearpygui()
