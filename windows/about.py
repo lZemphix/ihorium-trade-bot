099243dc9d38f6755c77055eba7ad38e01671f18
Zemphix <vafinaaa51@gmail.com>
2024-06-14 00:37:55 +0500

    data was updated

--- windows/about.py
+++ windows/about.py
@@ -3,7 +3,7 @@ from callbacks.callbacks import *
 
 def about_window():
     with dpg.texture_registry():
-        width, height, channels, data = dpg.load_image("src/logo.png")
+        width, height, channels, data = dpg.load_image("src/images/logo.png")
         texture_id = dpg.add_static_texture(width, height, data)
     with dpg.window(label="About", pos=[400,250], width=450, height=300, tag='about_window_tag'):        
         dpg.add_image(texture_id)
